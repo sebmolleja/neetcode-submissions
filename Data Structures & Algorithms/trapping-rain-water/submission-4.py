072a3247df7126cd430dff27c7da923aa8e36d59
sebmolleja <120436286+sebmolleja@users.noreply.github.com>
2026-08-04 19:57:29 -0400

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        area = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                area += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                area += rightMax - height[r]
+        
+        return area
+
+
+
+    """
+    left pointer and leftMax / right pointer and rightMax
+    area += leftMax - height[l]
+    two sides opposite dir
+    """
