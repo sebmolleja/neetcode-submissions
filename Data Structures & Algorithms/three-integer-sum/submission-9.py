6069f5ef942f32e00f84b3b41759ded466e88619
sebmolleja <120436286+sebmolleja@users.noreply.github.com>
2026-08-14 22:12:31 -0400

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.py
@@ -0,0 +1,37 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ret = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    ret.append([nums[i], nums[l], nums[r]])
+                    l += 1
+
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+            
+        return ret
+
+
+    """
+    for loop walks 
+    rest of array run i + 1 = l and end with r and run 2sum on sorted.
+    need to check dupes adjacent to values i and l
+
+
+    [-1,0,1,2,-1,-4]
+
+    """
