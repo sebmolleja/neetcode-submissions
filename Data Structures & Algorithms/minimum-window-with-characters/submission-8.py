b1017656aa27e5ddfc5037d8a9d2eda6f01dbda5
sebmolleja <120436286+sebmolleja@users.noreply.github.com>
2026-08-17 09:57:16 -0400

    Add: minimum-window-with-characters - submission-8

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-8.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        l, shortest = 0, float('inf')
+        min_substring = ""
+        freq_s, freq_t = {}, {}
+
+        for char in t:
+            freq_t[char] = freq_t.get(char, 0) + 1
+
+        have, need = 0, len(freq_t)
+
+        for r in range(len(s)):
+            if s[r] in freq_t:
+                freq_s[s[r]] = freq_s.get(s[r], 0) + 1
+                if freq_s[s[r]] == freq_t[s[r]]:
+                    have += 1
+
+            while have == need:
+                if r - l + 1 < shortest:
+                    shortest = min(shortest, r - l + 1)
+                    min_substring = s[l:r + 1]
+                
+                if s[l] in freq_t:
+                    freq_s[s[l]] -= 1
+                    if freq_s[s[l]] < freq_t[s[l]]:
+                        have -= 1
+                
+                l += 1
+        
+        return min_substring if shortest != float('inf') else ""
+
+
+
+    """
+    shortest pattern in while get shortest
+
+    use have need and freq maps
+
+    OUZODYXAZV
+    ^
+     ^
+
+
+    """
