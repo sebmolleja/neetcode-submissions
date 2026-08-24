3e6e6a0fae11bc50e55b53a2b7793593ed7217c5
sebmolleja <120436286+sebmolleja@users.noreply.github.com>
2026-08-24 13:31:59 -0400

    Add: permutation-string - submission-15

---
+++ Data Structures & Algorithms/permutation-string/submission-15.py
@@ -0,0 +1,48 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        freq_s1, freq_s2 = {}, {}
+
+        for char in s1:
+            freq_s1[char] = freq_s1.get(char, 0) + 1
+
+        for r in range(len(s2)):
+            freq_s2[s2[r]] = freq_s2.get(s2[r], 0) + 1
+
+            while r - l + 1 == len(s1):
+                if freq_s1 == freq_s2:
+                    return True
+                
+                freq_s2[s2[l]] -= 1
+                if freq_s2[s2[l]] == 0:
+                    del freq_s2[s2[l]]
+                
+                l += 1
+        
+        return False
+                
+
+                
+
+
+
+        """
+        s1 = abc
+
+        a: 1
+        b : 1
+        c : 1
+
+        length = 3
+
+        lecabee
+            ^
+          ^
+
+        c : 1
+        a : 1
+        b : 1
+
+
+        """
+        
